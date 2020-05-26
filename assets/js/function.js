4916f3279e95b04b2ce1efe7ebc107bfc46d2c61
Amava Lin <saramlin10@gmail.com>
2020-05-26 17:32:09 -0400

    mobile view

--- assets/js/function.js
+++ assets/js/function.js
@@ -3,7 +3,7 @@ $(function() {
   $(window).scroll(function() {
     var scroll = $(this).scrollTop();
 
-    if (scroll >= $('.topBarTrigger').offset().top) {
+    if ($('.topBarTrigger').offset() && scroll >= $('.topBarTrigger').offset().top) {
         $('.topBar').removeClass('hidden');
     } else {
         $('.topBar').addClass('hidden');
@@ -17,5 +17,20 @@ $(document).ready(function() {
     $('.slideout-menu').toggleClass('slideout-open');
     $('main').toggleClass('pushLeft');
     $('.bar').toggleClass('open');
+    $('.toggle-button').addClass('hidden');
+    $('.toggle-button-2').addClass('pushLeft');
+    $('.toggle-button-2').removeClass('hidden');
+  });
+});
+
+$(document).ready(function() {
+	$('.toggle-button-2').click(function(){
+    $('.slideout-menu').toggleClass('slideout-open');
+    $('main').toggleClass('pushLeft');
+    $('.bar').toggleClass('open');
+    $('.toggle-button').removeClass('hidden');
+    $('.toggle-button-2').addClass('hidden');
+    $('.toggle-button-2').removeClass('pushLeft');
+  
   });
 });
